86144218e34e1987c5fbb258f9611355a162b335
Lucas Garcia Bertaina <lucasgarciabertaina@gmail.com>
2023-10-18 17:31:07 +1300

    1.2 Variables and Arithmetic Expressions: Exercise 1-3

--- index.c
+++ index.c
@@ -12,10 +12,13 @@ main()
   upper = 300; /* upper limit */
   step = 20;  /* step size */
 
+  printf("_____________________\n| Celcius |   Fahr  |\n#####################\n");
+
   fahr = lower;
   while (fahr <= upper) {
     celsius = (5.0/9.0) * (fahr-32.0);
-    printf("%3.0f %6.1f\n", fahr, celsius);
+    printf("|   %3.0f   | %6.1f  |\n", fahr, celsius);
     fahr = fahr + step;
   }
+  printf("---------------------\n");
 }
