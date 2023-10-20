78ab4cfc933c2e132cb64ebf9952d68972b057e5
Lucas Garcia Bertaina <lucasgarciabertaina@gmail.com>
2023-10-20 19:23:32 +1300

    1.5.1 File Copying: Exercise 1-7

--- index.c
+++ index.c
@@ -3,12 +3,5 @@
 /* copy input to output; 2nd version */
 main()
 {
-  int c;
-  int condition;
-
-  c = getchar();
-  while (condition = getchar() != EOF) {
-    printf("condition %d\n", condition);
-    putchar(c);
-  }
+  printf("EOF %d", EOF);
 }
