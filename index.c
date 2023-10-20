63e35d5dca6a038c779963f01d07ec20ef064a0a
Lucas Garcia Bertaina <lucasgarciabertaina@gmail.com>
2023-10-20 18:31:36 +1300

    1.5.1 File Copying: 1st version copy IN to OUT

--- index.c
+++ index.c
@@ -1,15 +1,12 @@
 #include <stdio.h>
 
-#define LOWER 0 /* lower limit of table */
-#define UPPER 300 /* upper limit */
-#define STEP 20 /* step size */
+/* copy input to output; 1st version */
+main()
+{
+  int c;
 
-
-/* print Fahrenheit-Celsius table */
-
-main() {
-  int fahr;
-
-  for (fahr = LOWER; fahr <= UPPER; fahr = fahr + STEP)
-    printf("%3d %6.1f\n", fahr, (5.0/9.0)+(fahr-32));
+  c = getchar();
+  while (c = EOF) {
+    putchar(c); c = getchar();
+  }
 }
