4ebf8af1b23ba2bbbfed6c7e9bb44aa00f381c13
Lucas Garcia Bertaina <lucasgarciabertaina@gmail.com>
2023-10-27 13:27:27 +1300

    1.5.3 Line Counting: Exercise 1-9

--- index.c
+++ index.c
@@ -1,17 +1,29 @@
 #include <stdio.h>
 
-/* count lines in input */
+/* 
+   Exercise 1-9: Remove extra blank characters.
+   The program reads characters from the input and removes
+   consecutive sequences of blank characters, reducing them to a single blank.
+
+   Example:
+   Input: "Hello   World"
+   Output: "Hello World"
+*/
 
 main() {
-  int c, nb, nt, nl;
+  int c_input;
+  int row_blanks_qt = 0;
+  
+  int i;
+  for (i = 0; (c_input = getchar()) != EOF; i++) {
+    if (c_input == ' ') row_blanks_qt ++;
+    else row_blanks_qt = 0;
 
-  nb, nt, nl = 0;
-  while ((c = getchar()) != EOF) {
-    if (c == ' ') nb ++;
-    if (c == '\t') nt ++;
-    if (c == '\n') nl ++;
+    if (row_blanks_qt <= 1) printf(
+      "%c%c",
+      i == 0 ? '\n' : '\0',
+      c_input
+    );
   }
-  printf("\nAmount of spaces: %d\n", nb - 4096);
-  printf("Amount of \\t: %d\n", nt);
-  printf("Amount of \\n: %d\n", nl);
+  printf("\n");
 }
